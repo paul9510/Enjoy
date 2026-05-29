04d43e8def2fb9d947edf9d0adb2e17df9aefa9b
paul9510 <pminiou@lerebours.fr>
2026-05-29 07:42:50 +0200

    first commit

---
+++ src/test/java/org/example/enjoy/EnjoyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.enjoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnjoyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
